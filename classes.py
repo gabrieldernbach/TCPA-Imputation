111823be86b8e1f36b0a34c294b37815af34bdd4
gbd <mail@gabrieldernbach.de>
2020-09-28 15:36:17 +0200

    big Classes

--- classes.py
+++ classes.py
@@ -4,9 +4,7 @@ import torch as tc
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 import random
-import numpy as np
-import math
-
+import torch.nn.functional as F
 
 class Mydataset(Dataset):
     def __init__(self, data, p):
@@ -24,12 +22,12 @@ class Mydataset(Dataset):
         else:
             q = self.p
 
-        mask = tc.bernoulli(tc.ones(self.tensor_size)*q)==0
+        mask = tc.bernoulli(tc.ones(self.tensor_size)*q) == 0
         masked_data = target.clone()
         masked_data[mask]= float('nan')
         return masked_data.float(), target.float()
 
-def bn_linear(input_dim, output_dim):
+def bn_linear(input_dim: int, output_dim: int):
     return nn.Sequential(
         nn.BatchNorm1d(input_dim),
         nn.Linear(input_dim, output_dim, bias=False)
@@ -37,44 +35,32 @@ def bn_linear(input_dim, output_dim):
 
 ######################################################################
 class ResBlock(nn.Module):
-    def __init__(self, input_dim, width, activation, act_bool):
+    def __init__(self, input_dim, width,  act_bool, activation=nn.ReLU()):
         super(ResBlock, self).__init__()
+
         self.alpha = nn.Parameter(torch.tensor(1.0, requires_grad=True))
         self.input_dim, self.act_bool = input_dim, act_bool
-        self.layers1 = nn.Sequential(
-            bn_linear(input_dim, width[0]),
+        self.layers = nn.Sequential(
+            bn_linear(input_dim, width),
             activation,
             nn.Dropout(p=0.2),
-            bn_linear(width[0], input_dim),
+            bn_linear(width, input_dim),
             activation if act_bool else nn.Identity()
         )
-        self.layers2 = nn.Sequential(
-            bn_linear(input_dim, width[1]),
-            activation,
-            nn.Dropout(p=0.2),
-            bn_linear(width[1], input_dim),
-            activation if act_bool else nn.Identity()
-        )
-        self.layers3 = nn.Sequential(
-            bn_linear(input_dim, width[2]),
-            activation,
-            nn.Dropout(p=0.2),
-            bn_linear(width[2], input_dim),
-            activation if act_bool else nn.Identity()
-        )
+
     def forward(self,x):
         residual = x.clone() if self.act_bool else tc.zeros(x.shape).cuda()
-        return residual*self.alpha + self.layers1(x) + self.layers2(x)+ self.layers3(x)
+        return residual*self.alpha + self.layers(x)
 
 
 class ResNet(nn.Module):
-    def __init__(self, input_dim, width, depth, activation):
+    def __init__(self, input_dim, width, depth, variational, SHAP= False):
         super(ResNet,self).__init__()
+        self.SHAP = SHAP
         self.init_nan = nn.Parameter(torch.tensor(0.0, requires_grad=True))
         self.layers = nn.Sequential(
-            *[ResBlock(input_dim, width, activation, act_bool=True) for _ in range(depth)],
-            ResBlock(input_dim, width, activation, act_bool= False)
-
+            *[ResBlock(input_dim, width, act_bool=True) for _ in range(depth)],
+            ResBlock(input_dim, width, act_bool= False)
         )
 
     def forward(self, x, repeats = 10):
@@ -86,49 +72,54 @@ class ResNet(nn.Module):
 
         for i in range(repeats):
             y = self.layers(y)
-            y_history.append(y)
             y[notnans] = x[notnans]
+            y_history.append(y)
+        if self.SHAP: return(y)
+        else:
+            return(y, y_history, nans)
 
-        return(y, y_history, nans)
 
+class VariationalResNet(ResNet):
+    def __init__(self, input_dim, width, depth, variational, SHAP= False):
+        super(ResNet,self).__init__()
+        self.SHAP = SHAP
+        self.variational = variational
+        self.init_nan = nn.Parameter(torch.tensor(0.0, requires_grad=True))
 
-
-#####################################################################
-class block(nn.Module):
-    def __init__(self, input_dim, output_dim, relu, activation, normalize):
-        super(block, self).__init__()
-        self.relu = relu
-        self.layers = nn.Sequential(
-            bn_linear(input_dim, output_dim) if normalize else nn.Linear(input_dim, output_dim),
-            activation
-        ) if relu else nn.Linear(input_dim, output_dim)
-
-    def forward(self,x):
-        return self.layers(x)
-
-class Net(nn.Module):
-    def __init__(self, layer_sizes, relu, activation = nn.ReLU, normalize = False):
-        super(Net,self).__init__()
-
-        self.layers = nn.Sequential(
-            *[block(inp, outp, relu[i], activation, normalize) for i, (inp,outp) in enumerate(zip(layer_sizes, layer_sizes[1:]))]
+        self.enc = nn.Sequential(
+            *[ResBlock(input_dim, width, act_bool=True) for _ in range(depth-1)]
         )
-    def forward(self, x, repeats = 10, init_nan= 0):
+
+        self.mean_layer = nn.Linear(input_dim, width)
+        self.logvar_layer = nn.Linear(input_dim, width)
+
+        self.dec = nn.Sequential(
+            nn.Linear(width, input_dim),
+            nn.ReLU(),
+            ResBlock(input_dim, width, act_bool=False)
+        )
+
+    def sample(self, mean, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mean + eps * std
+
+    def forward(self, x, repeats = 10):
         y = x.clone()
         nans = tc.isnan(x)
         notnans = ~nans
-        y[nans] = init_nan
-
+        y[nans] = self.init_nan
         y_history = []
+
         for i in range(repeats):
-            y = self.layers(y)
-            y_history.append(y)
+            y = self.enc(y)
+            mean_ = self.mean_layer(y)
+            var_ = self.logvar_layer(y)
+            latent = self.sample(mean_, var_)
+            y = self.dec(latent)
             y[notnans] = x[notnans]
-
-        return(y, y_history, nans)
-
-
-
-#################################################################################################
-
-
+            y_history.append(y)
+        if self.SHAP:
+            return y
+        else:
+            return(y, y_history, nans)
