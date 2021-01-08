be503219ef7af34cf5c86ea7fc91b37c0b55cd63
PhGK <philipp-keyl@gmx.net>
2021-01-08 15:25:44 +0100

    second Version

---
+++ Data_Shapley_V1.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import torch as tc
+from sklearn import datasets
+import sklearn
+
+
+def get_data(dataname):
+    if dataname == "protein":
+        DATAPATH = '../data2/TCPA_data_sel.csv'
+        #import data from PATH
+        data_with_names = pd.read_csv(DATAPATH)
+        ID, data = data_with_names.iloc[:,:2], tc.tensor(data_with_names.iloc[:,2:].values)
+        protein_names = data_with_names.columns[2:]
+        tc.manual_seed(0)
+        randomized_data = data[tc.randperm(data.shape[0]), :].float()
+
+    elif dataname == 'iris':
+        iris,_ = datasets.load_iris(return_X_y=True)
+        randomized_data = tc.tensor(sklearn.preprocessing.scale(iris))
+
+        boston,_ = datasets.load_boston(return_X_y=True)
+        randomized_data = tc.tensor(sklearn.preprocessing.scale(boston))
+
+    elif dataname == 'breast_cancer':
+        breast_cancer,_ = datasets.load_breast_cancer(return_X_y=True)
+        breast_cancer = sklearn.preprocessing.scale(breast_cancer)
+        randomized_data = tc.tensor(sklearn.preprocessing.scale(breast_cancer))
+
+    elif dataname == 'wine':
+        wine,_ = datasets.load_wine(return_X_y=True)
+        randomized_data = tc.tensor(sklearn.preprocessing.scale(wine))
+
+    return randomized_data
