234ee17dabaac9057738d8ef73303db32fd32863
PhGK <philipp-keyl@gmx.net>
2021-03-25 15:14:59 +0100

    wip

--- Main_Shapley_V2.py
+++ Main_Shapley_V2.py
@@ -45,7 +45,7 @@ if train_network:
                 gibbs_sampler = model.GibbsSampler(neuralnet=vae, warm_up=4, convergence=0.0, result_path='results', device = device)
                 #train and test model in n fold crossvalidation
                 model.cross_validate(model=gibbs_sampler, train_data=train_set, test_data = test_set, path = 'results', train_epochs = 501, lr = 0.0001, train_repeats = 15, batch_factor=1, ncrossval=1)
-
+    print('training finished')
 
 if conditional:
     gibbs_sampler = tc.load('results/trained_model/Gibbs_sampler_trainepochs={}_var={}_k={}_{}.pt'.format(load_epoch, load_variational, load_k, load_lin)) # save and load always gibbs_sampler or model within?
