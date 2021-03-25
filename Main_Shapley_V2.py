bd2fd96bc12dc6151ecaecfd23c3bf1766623fd9
PhGK <philipp-keyl@gmx.net>
2021-03-25 15:56:41 +0100

    wip

--- Main_Shapley_V2.py
+++ Main_Shapley_V2.py
@@ -25,7 +25,7 @@ calc_single_shapley = False
 counterfactual = False
 conditional = False
 
-load_epoch, load_variational, load_k, load_lin = 500, True, 5, 'nonlinear' #define model that shall be loaded for shapley
+load_epoch, load_variational, load_k, load_lin = 1000, True, 5, 'nonlinear' #define model that shall be loaded for shapley
 ##################
 plot = False
 
@@ -44,7 +44,7 @@ if train_network:
                 #init gibb sampler with neural network
                 gibbs_sampler = model.GibbsSampler(neuralnet=vae, warm_up=4, convergence=0.0, result_path='results', device = device)
                 #train and test model in n fold crossvalidation
-                model.cross_validate(model=gibbs_sampler, train_data=train_set, test_data = test_set, path = 'results', train_epochs = 501, lr = 0.0001, train_repeats = 15, batch_factor=1, ncrossval=1)
+                model.cross_validate(model=gibbs_sampler, train_data=train_set, test_data = test_set, path = 'results', train_epochs = 1001, lr = 0.0001, train_repeats = 15, batch_factor=1, ncrossval=1)
     print('training finished')
 
 if conditional:
