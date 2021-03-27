ae62fb19ffe65cf12f1ec74618febb736975a161
PhGK <philipp-keyl@gmx.net>
2021-03-27 13:53:17 +0100

    wip

--- triangleshapley_Shapley_V1.py
+++ triangleshapley_Shapley_V1.py
@@ -88,7 +88,7 @@ class Shapley:
                 counter += specific
                 convergencechecker.append(meandiff)
 
-            if all(abs(convergencechecker[-2] - convergencechecker[-1])<0.00001) and t >500:
+            if all(abs(convergencechecker[-2] - convergencechecker[-1])<0.000001) and t >500:
                 #break if consequent meanvalues are not different
                 print(p, 'converged at', len(convergencechecker))
                 break
