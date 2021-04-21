59c8ba16151f16b7fc30384c627378fd5877fc27
PhGK <philipp-keyl@gmx.net>
2021-04-21 17:50:34 +0200

    wip

--- Main_Shapley_V2.py
+++ Main_Shapley_V2.py
@@ -26,7 +26,7 @@ calc_single_shapley = False
 counterfactual = False
 conditional = False
 triangle=False
-load_epoch, load_variational, load_k, load_lin = 5, False, 1, 'nonlinear' #define model that shall be loaded for shapley
+load_epoch, load_variational, load_k, load_lin = 500, False, 1, 'nonlinear' #define model that shall be loaded for shapley
 ##################
 
 train_set, test_set, protein_names = data_sh.get_data('beeline')
@@ -40,11 +40,11 @@ if train_network:
         for nonlinear in [True]:
             for k in [1]:
                 #specify neural network
-                vae = model.VAE(input_dim=train_set.size(1), width=train_set.size(1)*16, sample_width=train_set.size(1)*16, depth=10, variational = variational, nonlinear = True, k = k)
+                vae = model.VAE(input_dim=train_set.size(1), width=train_set.size(1)*16, sample_width=train_set.size(1)*16, depth=8, variational = variational, nonlinear = True, k = k)
                 #init gibb sampler with neural network
                 gibbs_sampler = model.GibbsSampler(neuralnet=vae, warm_up=6, convergence=0.0, result_path='results', device = device)
                 #train and test model in n fold crossvalidation
-                model.cross_validate(model=gibbs_sampler, train_data=train_set, test_data = test_set, path = 'results', train_epochs = 5, lr = 0.0001, train_repeats = 15, batch_factor=1, ncrossval=1)
+                model.cross_validate(model=gibbs_sampler, train_data=train_set, test_data = test_set, path = 'results', train_epochs = 500, lr = 0.0001, train_repeats = 15, batch_factor=1, ncrossval=1)
     print('training finished')
 
 if conditional:
