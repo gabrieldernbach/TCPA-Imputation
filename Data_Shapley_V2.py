c574dd98561128e9d5a4bf46f498ce9f7a1141c9
PhGK <philipp-keyl@gmx.net>
2021-05-25 11:11:44 +0200

    wip

--- Data_Shapley_V2.py
+++ Data_Shapley_V2.py
@@ -88,7 +88,7 @@ def get_data(dataname):
 
 
     elif dataname == 'beeline':
-        datapath = '/home/pkeyl/Projekte/Shapley/data10/Synthetic/dyn-BFC/'
+        datapath = '/mnt/scratch2/mlprot/Projekte/Shapley/data10/Synthetic/dyn-BFC/'
         data = pd.read_csv(datapath + 'ExpressionData.csv')
         protein_names = list(data.iloc[:,0])
         data = np.array((data.iloc[:,1:]))
