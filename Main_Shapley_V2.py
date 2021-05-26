cf8ee00405dd185df83fb692a4af4eb8c8b533fc
PhGK <philipp-keyl@gmx.net>
2021-05-26 13:56:34 +0200

    wip

--- Main_Shapley_V2.py
+++ Main_Shapley_V2.py
@@ -54,7 +54,7 @@ if calc_shapley:
     gibbs_sampler = tc.load('results/trained_model/Gibbs_sampler_trainepochs={}_var={}_k={}_{}.pt'.format(load_epoch, load_variational, load_k, load_lin)) # save and load always gibbs_sampler or model within?
     gibbs_sampler.device = device
     shapley = sh.Shapley(gibbs_sampler, data = test_set, protein_names= protein_names, device=device, datatype = datatype)
-    shapley.calc_all(device=device, steps=5001)
+    shapley.calc_all(device=device, steps=2001)
 
 if calc_hsic:
     gibbs_sampler = tc.load('results/trained_model/Gibbs_sampler_trainepochs={}_var={}_k={}_{}.pt'.format(load_epoch, load_variational, load_k, load_lin)) # save and load always gibbs_sampler or model within?
