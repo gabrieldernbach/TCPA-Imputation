6c1330ed352a329668bd057367bed8aeb740412c
PhGK <philipp-keyl@gmx.net>
2021-05-28 16:02:05 +0200

    wip

--- startprograms.py
+++ startprograms.py
@@ -1,7 +1,7 @@
 import subprocess
 names = ['BFC', 'CY', 'LI', 'LL', 'BF', 'TF']
 
-process_list = [subprocess.Popen(['screen python3', name]) for name in names]
+process_list = [subprocess.Popen(['python3', 'Main_Shapley_V2.py', name]) for name in names]
 
 for proc in process_list:
     proc.wait()
