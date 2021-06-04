5d608d18b5ec3664ebbeddf36d718fd6e02061b3
PhGK <philipp-keyl@gmx.net>
2021-06-04 23:33:17 +0200

    wip

--- Main_Shapley_V2.py
+++ Main_Shapley_V2.py
@@ -23,11 +23,11 @@ for folder in ('results','results/figures', 'results/log', 'results/trained_mode
         os.makedirs(folder)
 device = tc.device('cuda:0')
 
-train_network = False
+train_network = True
 calc_shapley = True
 calc_hsic = True
 
-load_epoch, load_variational, load_k, load_lin =1006000, True, 1, 'nonlinear' #define model that shall be loaded for shapley
+load_epoch, load_variational, load_k, load_lin =6000, False, 1, 'nonlinear' #define model that shall be loaded for shapley
 ##################
 
 train_set, test_set, protein_names = data_sh.get_data(datatype)
@@ -37,7 +37,7 @@ print(train_set.shape)
 print(protein_names)
 
 if train_network:
-    for variational in [True]:
+    for variational in [False]:
         for nonlinear in [True]:
             for k in [1]:
                 #specify neural network
@@ -48,7 +48,7 @@ if train_network:
                 #train and test model in n fold crossvalidation
                 model.cross_validate(model=gibbs_sampler, train_data=train_set, test_data = test_set, path = 'results', train_epochs = 6001, lr = 0.00001, train_repeats =20, batch_factor=1, ncrossval=1)
     print('training finished')
-
+'''
 gibbs_sampler = model.SimpleModel(nfeatures = train_set.size(1), hidden = 100*train_set.size(1), depth = 10)
 for i in range(100):
     model.train(gibbs_sampler, trainset=train_set, testset = test_set, lr = 0.0001, device=device)
@@ -56,12 +56,12 @@ for i in range(100):
     model.train(gibbs_sampler, trainset=train_set, testset = test_set, lr = 0.00001, device=device)
 for i in range(100):
     model.train(gibbs_sampler, trainset=train_set, testset=test_set, lr=0.000001, device=device)
-
+'''
 if calc_hsic:
     #gibbs_sampler = tc.load('results/trained_model/Gibbs_sampler_trainepochs={}_var={}_k={}_{}.pt'.format(load_epoch, load_variational, load_k, load_lin)) # save and load always gibbs_sampler or model within?
     gibbs_sampler.device = device
     shapley = hsic.Shapley(gibbs_sampler, data = test_set, protein_names= protein_names, device=device, datatype = datatype)
-    shapley.calc_all(device=device, steps=200)
+    shapley.calc_all(device=device, steps=400)
 
 if calc_shapley:
     #gibbs_sampler = tc.load('results/trained_model/Gibbs_sampler_trainepochs={}_var={}_k={}_{}.pt'.format(load_epoch, load_variational, load_k, load_lin)) # save and load always gibbs_sampler or model within?
