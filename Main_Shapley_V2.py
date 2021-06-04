1989112d041c1c596bc86a258dce4451a7eca882
PhGK <philipp-keyl@gmx.net>
2021-06-04 23:41:02 +0200

    wip

--- Main_Shapley_V2.py
+++ Main_Shapley_V2.py
@@ -41,7 +41,7 @@ if train_network:
         for nonlinear in [True]:
             for k in [1]:
                 #specify neural network
-                vae = model.VAE(input_dim=train_set.size(1), width=train_set.size(1)*32, sample_width=train_set.size(1)*128, depth=12, variational = variational, nonlinear = True, k = k)
+                vae = model.VAE(input_dim=train_set.size(1), width=train_set.size(1)*32, sample_width=train_set.size(1)*128, depth=8, variational = variational, nonlinear = True, k = k)
                 #vae = model.MlpMix(reps=10, n_samp=256, hidden=128, ins= train_set.size(1))
                 #init gibb sampler with neural network
                 gibbs_sampler = model.GibbsSampler(neuralnet=vae, warm_up=5, convergence=0.0, result_path='results', device = device)
